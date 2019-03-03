650c38bb62a2db0e43c9944b0ae22b810c645f5e
tamberg <tamberg@yaler.net>
2019-03-03 22:33:28 +0100

    Added example code.

---
+++ 03/Arduino/ESP8266_NTPClient/ESP8266_NTPClient.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+const char *ssid = "MY_SSID"; // TODO
+const char *password = "MY_PASSWORD"; // TODO
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\nConnecting to network ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100); // keeps watchdog happy
+  }
+  Serial.print("Connected to network, local IP = "); 
+  Serial.println(WiFi.localIP());
+
+  // call built-in, ESP8266-specific function in time.c
+  // see https://www.esp8266.com/viewtopic.php?t=17422    
+  const int timezone = 0;
+  const int dst = 0;
+  configTime(timezone * 3600, dst, "pool.ntp.org", "time.nist.gov");
+  // wait for time() being adjusted, as a side effect of configTime
+  while (time(NULL) < 28800 * 2) {
+    delay(100);
+  }
+}
+
+void loop() {
+  // number of seconds elapsed since the Epoch, 
+  // 1970-01-01 00:00:00 +0000 (UTC).
+  time_t now = time(NULL);
+
+  // http://man7.org/linux/man-pages/man3/ctime.3.html
+  // same as asctime(localtime(&now))
+  Serial.print(ctime(&now));
+
+  // ISO 8601 UTC, e.g. 2019-03-05T00:00:00Z
+  const char *format = "%Y-%m-%dT%H:%M:%SZ";
+  const struct tm *t = gmtime(&now);
+  const size_t maxLen = 21;
+  char s[maxLen];
+  size_t len = strftime(s, maxLen, format, t);
+  Serial.println(s);
+  
+  delay(1000);
+}
