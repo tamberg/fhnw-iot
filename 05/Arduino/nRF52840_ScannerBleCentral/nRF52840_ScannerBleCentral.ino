805ef1d99ea50d500417941ffe66abb0c3eccb57
tamberg <tamberg@yaler.net>
2019-03-17 13:03:13 +0100

    Cleanup.

--- 05/Arduino/nRF52840_ScannerBleCentral/nRF52840_ScannerBleCentral.ino
+++ 05/Arduino/nRF52840_ScannerBleCentral/nRF52840_ScannerBleCentral.ino
@@ -1,4 +1,4 @@
-// Heart Rate Monitor BLE peripheral. Copyright (c) Thomas Amberg, FHNW
+// Scanner BLE central. Copyright (c) Thomas Amberg, FHNW
 
 // Based on https://github.com/adafruit/Adafruit_nRF52_Arduino
 // /tree/master/libraries/Bluefruit52Lib/examples/Central
