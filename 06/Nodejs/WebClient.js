150975ed75938c508e482f93e15a5af7f22da272
tamberg <tamberg@yaler.net>
2019-03-22 10:52:43 +0000

    Added example code.

---
+++ 06/Nodejs/WebClient.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.get('http://tmb.gr/hello.html', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
---
+++ 06/Nodejs/WebService.js
@@ -0,0 +1,18 @@
+
+const http = require('http');
+
+//const host = '127.0.0.1'; // loopback address
+//const host = '192.168.2.23'; // local address
+const host = '0.0.0.0'; // any incoming address
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('It works!\n');
+});
+
+server.listen(port, host, () => {
+  console.log(`Server running on port ${port}`);
+});
