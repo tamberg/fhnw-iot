cf7782f8963cf6ea856ec4ea79b18cb172126659
tamberg <tamberg@yaler.net>
2019-03-24 17:01:18 +0000

    Extended example code for readability and robustness.

--- 06/Nodejs/BleScan.js
+++ 06/Nodejs/BleScan.js
@@ -1,8 +1,19 @@
 const noble = require("noble");
 
 noble.on("discover", function(peripheral) {
-  //console.log("found peripheral:", peripheral);
-  console.log("found peripheral:", peripheral.advertisement.localName);
+  //console.log(peripheral); // lots of information
+  console.log(
+    peripheral.address + ", " +
+    peripheral.advertisement.localName);
 });
 
-noble.startScanning(["180d"], true);
+noble.on("stateChange", function(state) {
+  console.log("state changed to: ", state);
+  if ("poweredOn" == state) {
+    console.log("scanning...");
+    //noble.startScanning([], true); // all services
+    noble.startScanning(["180d"], false); // HRM service only
+  } else {
+    noble.stopScanning();
+  }
+});
