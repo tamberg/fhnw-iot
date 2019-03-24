a82512be21f99b974251f7f258308b485a8c152f
tamberg <tamberg@yaler.net>
2019-03-24 12:19:13 +0000

    Cleanup.

--- 06/Nodejs/WebService.js
+++ 06/Nodejs/WebService.js
@@ -1,17 +1,17 @@
-const http = require('http');
+const http = require("http");
 
-//const host = '127.0.0.1'; // loopback address
-//const host = '192.168.2.23'; // local address
-const host = '0.0.0.0'; // any incoming address
+//const host = "127.0.0.1"; // loopback address
+//const host = "192.168.2.23"; // local address
+const host = "0.0.0.0"; // any incoming address
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('It works!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("It works!\n");
 });
 
 server.listen(port, host, () => {
-  console.log(`Server running on port ${port}`);
+  console.log("Server running on port " + port);
 });
