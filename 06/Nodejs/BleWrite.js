070501a0612cae7f73b3f0657e0d6354049120ce
tamberg <tamberg@yaler.net>
2019-03-24 18:30:19 +0000

    Added example code.

---
+++ 06/Nodejs/BleWrite.js
@@ -0,0 +1,38 @@
+const noble = require("noble");
+
+const hrmServiceUuid = "180d";
+const hrmHeartRateCharacteristicUuid = "2a37";
+const serviceUuids = [hrmServiceUuid];
+const characteristicUuids = [hrmHeartRateCharacteristicUuid];
+
+noble.on("discover", (peripheral) => {
+  noble.stopScanning();
+  console.log(
+    peripheral.address + ", " +
+    peripheral.advertisement.localName);
+  peripheral.connect((err) => {
+    console.log("connected");
+    peripheral.discoverServices(serviceUuids, (err, services) => {
+      services.forEach((service) => {
+        console.log("found service:", service.uuid);
+        service.discoverCharacteristics(characteristicUuids, (err, characteristics) => {
+          characteristics.forEach((characteristic) => {
+            console.log("found characteristic:", characteristic.uuid);
+// TODO: write
+//            characteristic.read((error, data) => {
+//              const value = data.readUInt8(0);
+//              console.log("read characteristic value:", value);
+              peripheral.disconnect((err) => {
+                console.log("disconnected");
+                process.exit();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+console.log("scanning...");
+noble.startScanning(serviceUuids, false);
