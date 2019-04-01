f553dbed1798b561bad350844ce2e45188dac2c9
tamberg <tamberg@yaler.net>
2019-04-01 10:42:43 +0200

    Added example code.

---
+++ 07/Arduino/ESP8266_MqttPubClient/ESP8266_MqttPubClient.ino
@@ -0,0 +1,34 @@
+#include <ESP8266WiFi.h> // v2.4.2
+#include <ESP8266MQTTClient.h> // v1.0.4
+
+const char *ssid = "MY_SSID"; // TODO
+const char *password = "MY_PASSWORD"; // TODO
+
+MQTTClient client;
+
+void handleConnected() {
+  Serial.println("Connected to broker");
+  client.publish("hello", "Hello, World!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\nConnecting to network ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100); // keeps watchdog happy
+  }
+  Serial.print("Connected to network, local IP = "); 
+  Serial.println(WiFi.localIP());
+
+  client.onConnect(handleConnected);
+  client.onSubscribe(handleSubscribed);
+  client.onData(handleDataReceived);
+  client.begin("mqtt://test.mosquitto.org/");
+}
+
+void loop() {
+  client.handle();
+}
---
+++ 07/Arduino/ESP8266_MqttSubClient/ESP8266_MqttSubClient.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h> // v2.4.2
+#include <ESP8266MQTTClient.h> // v1.0.4
+
+const char *ssid = "MY_SSID"; // TODO
+const char *password = "MY_PASSWORD"; // TODO
+
+MQTTClient client;
+
+void handleConnected() {
+  Serial.println("Connected to broker");
+  client.subscribe("hello");
+}
+
+void handleSubscribed(int topicId) {
+  Serial.println("Subscribed");
+}
+
+void handleDataReceived(String topic, String data, bool b) {
+  Serial.print("Received topic: ");
+  Serial.print(topic);
+  Serial.print(", data: ");
+  Serial.println(data);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\nConnecting to network ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100); // keeps watchdog happy
+  }
+  Serial.print("Connected to network, local IP = "); 
+  Serial.println(WiFi.localIP());
+
+  client.onConnect(handleConnected);
+  client.onSubscribe(handleSubscribed);
+  client.onData(handleDataReceived);
+  client.begin("mqtt://test.mosquitto.org/");
+}
+
+void loop() {
+  client.handle();
+}
