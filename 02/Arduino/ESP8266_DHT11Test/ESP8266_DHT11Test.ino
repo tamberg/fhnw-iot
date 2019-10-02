dc1a5696bbc9f38b3ccb88ec5575834927018bb5
tamberg <tamberg@yaler.net>
2019-10-02 16:25:49 +0200

    Cleanup.

--- 02/Arduino/ESP8266_DHT11Test/ESP8266_DHT11Test.ino
+++ 02/Arduino/ESP8266_DHT11Test/ESP8266_DHT11Test.ino
@@ -1,25 +1,33 @@
+// Test code for Grove DHT11 humidity/temperature sensor on ESP8266
+
+// https://github.com/tamberg/fhnw-iot/wiki/Grove-Sensors#temperature--humidity-sensor-dht11
+// https://github.com/tamberg/fhnw-iot/wiki/Feather-Huzzah-ESP8266
+// https://github.com/tamberg/fhnw-iot/wiki/Grove-Adapters#pinout
+
 #include "DHTesp.h"
 
-const int dhtPin = 5; // Grove adapter I2C_1 or _2 used as D6
-const DHTesp::DHT_MODEL_t dhtType = DHTesp::DHT11;
+#define DHT_PIN 5 // Grove adapter I2C_1 or _2 used as D6
+#define DHT_MODEL DHTesp::DHT11
 
 DHTesp dht;
 
 void setup() {
   Serial.begin(115200);
-  dht.setup(dhtPin, dhtType);
+  dht.setup(DHT_PIN, DHT_MODEL);
 }
 
 void loop() {
-  float humi = dht.getHumidity();
-  float temp = dht.getTemperature();
+  float humi = dht.getHumidity(); // %
+  float temp = dht.getTemperature(); // *C
 
-  Serial.print(dht.getStatusString());
-  Serial.print(", ");
-  Serial.print(humi, 1);
-  Serial.print(", ");
-  Serial.print(temp, 1);
-  Serial.print("\n");
+  if (isnan(humi) || isnan(temp)) {
+    Serial.print("Failed to read from DHT11 sensor");
+  } else {
+    Serial.print(humi, 1);
+    Serial.print(" %, ");
+    Serial.print(temp, 1);
+    Serial.print(" *C\n");
+  }
 
-  delay(dht.getMinimumSamplingPeriod() + 200);
+  delay(dht.getMinimumSamplingPeriod() + 200); // ms
 }
