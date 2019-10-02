45e3faf1de165f08921abe6f429da22d7a594242
tamberg <tamberg@yaler.net>
2019-10-02 09:58:24 +0200

    Fixed.

--- 03/Arduino/ESP8266_WiFiClientSecureFingerprint/ESP8266_WiFiClientSecureFingerprint.ino
+++ 03/Arduino/ESP8266_WiFiClientSecureFingerprint/ESP8266_WiFiClientSecureFingerprint.ino
@@ -1,17 +1,20 @@
+// To get the SHA-1 fingerprint, check the certificate in your browser or us the following command on Linux or MacOS:
+// openssl s_client -connect www.howsmyssl.com:443 < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout -in /dev/stdin
+
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 
 const char *ssid = "MY_SSID"; // TODO
 const char *password = "MY_PASSWORD"; // TODO
 
 const char *host = "www.howsmyssl.com"; // via @spiessa
 const char *fingerprint = // SHA-1, not very secure anymore
-  "67 FF 3D BC D3 59 E2 C0 BD 04 3B 8A 57 CB 72 7C 0A 20 F5 E4"; 
+  "DA 74 23 69 75 B1 20 9B D8 83 AB 98 DF 2F 02 52 C4 15 81 90";
 const char *path = "/a/check";
 const int port = 443;
 
 void setup() {
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   Serial.print("\nConnecting to network ");
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);
@@ -24,35 +27,32 @@ void setup() {
 
   Serial.print("Connecting to host ");
   Serial.println(host);
-  WiFiClientSecure client; // use TLS
+  BearSSL::WiFiClientSecure client; // use TLS
+  //client.allowSelfSignedCerts();
+  client.setFingerprint(fingerprint);
   if (client.connect(host, port)) {
 
     Serial.print("Verifying host fingerprint ");
     Serial.println(fingerprint);
-    if (client.verify(fingerprint, host)) {
+    Serial.println("Sending HTTP request");
+    client.print("GET ");
+    client.print(path);
+    client.print(" HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(host);
+    client.print("\r\n");
+    client.print("Connection: close\r\n\r\n");
 
-      Serial.println("Sending HTTP request");
-      client.print("GET ");
-      client.print(path);
-      client.print(" HTTP/1.1\r\n");
-      client.print("Host: ");
-      client.print(host);
-      client.print("\r\n");
-      client.print("Connection: close\r\n\r\n");
-  
-      Serial.println("Reading HTTP response\n");
-      while (client.connected() || client.available()) {
-        int ch = client.read();
-        while (ch >= 0) {
-            Serial.print((char) ch);
-            ch = client.read();
-        }
+    Serial.println("Reading HTTP response\n");
+    while (client.connected() || client.available()) {
+      int ch = client.read();
+      while (ch >= 0) {
+          Serial.print((char) ch);
+          ch = client.read();
       }
-    } else {
-      Serial.println("Certificate mismatch");
     }
   } else {
-    Serial.println("Cannot connect");
+    Serial.println("Cannot connect or certificate mismatch");
   }
 }
 
