550b66d74ae693429988672732d71ae803bc8e87
tamberg <tamberg@yaler.net>
2019-10-02 18:21:13 +0200

    Added sensor measurement.

--- 03/Arduino/ESP8266_ThingSpeakWiFiClientSecure/ESP8266_ThingSpeakWiFiClientSecure.ino
+++ 03/Arduino/ESP8266_ThingSpeakWiFiClientSecure/ESP8266_ThingSpeakWiFiClientSecure.ino
@@ -1,4 +1,8 @@
 #include <ESP8266WiFi.h>
+#include "DHTesp.h"
+
+const int dhtPin = 5; // Grove adapter I2C_1 or _2 used as D6
+const DHTesp::DHT_MODEL_t dhtModel = DHTesp::DHT11;
 
 const char *ssid = "MY_SSID"; // TODO
 const char *password = "MY_PASSWORD"; // TODO
@@ -9,9 +13,11 @@ const char *host = "api.thingspeak.com";
 const int port = 443;
 
 long t0 = 0;
+DHTesp dht;
 
 void setup() {
   Serial.begin(115200);
+  dht.setup(dhtPin, dhtModel);
   //Serial.setDebugOutput(true);
   Serial.print("\nConnecting to network ");
   Serial.println(ssid);
@@ -30,37 +36,41 @@ void loop() {
     t0 = t;
 
     // read sensor values
-    float temp = 23; // TODO
-    float humi = 42; // TODO
+    float humi = dht.getHumidity(); // %
+    float temp = dht.getTemperature(); // *C
 
-    // connect to remote host
-    BearSSL::WiFiClientSecure client; // use TLS
-    client.setInsecure(); // no cert validation
-    if (client.connect(host, port)) {
-  
-      // send HTTPS request
-      client.print(verb);
-      client.print(" ");
-      client.print(path);
-      client.print("&field1=");
-      client.print(temp);
-      client.print("&field2=");
-      client.print(humi);
-      client.print(" HTTP/1.1\r\n");
-      client.print("Host: ");
-      client.print(host);
-      client.print("\r\n");
-      client.print("Connection: close\r\n\r\n");
-  
-      // read HTTPS response
-      while (client.connected() || client.available()) {
-        int ch = client.read();
-        while (ch >= 0) {
-          Serial.print((char) ch);
-          ch = client.read();
+    if (isnan(humi) || isnan(temp)) {
+      Serial.print("Failed to read from DHT11 sensor\n");
+    } else {
+      // connect to remote host
+      BearSSL::WiFiClientSecure client; // use TLS
+      client.setInsecure(); // no cert validation
+      if (client.connect(host, port)) {
+    
+        // send HTTPS request
+        client.print(verb);
+        client.print(" ");
+        client.print(path);
+        client.print("&field1=");
+        client.print(temp);
+        client.print("&field2=");
+        client.print(humi);
+        client.print(" HTTP/1.1\r\n");
+        client.print("Host: ");
+        client.print(host);
+        client.print("\r\n");
+        client.print("Connection: close\r\n\r\n");
+    
+        // read HTTPS response
+        while (client.connected() || client.available()) {
+          int ch = client.read();
+          while (ch >= 0) {
+            Serial.print((char) ch);
+            ch = client.read();
+          }
         }
+        Serial.print("\n");
       }
-      Serial.print("\n");
     }
   }
 }
