5cf2ceb36f90f7258dbfdde9d6d9d336c0b43d2c
tamberg <tamberg@yaler.net>
2019-12-03 01:27:06 +0100

    Updated.

--- 11/Nodejs/FishTankAlexaSkill/index.js
+++ 11/Nodejs/FishTankAlexaSkill/index.js
@@ -1,50 +1,140 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
+// $ npm install ask-sdk-core
+// $ npm install ask-sdk-model
+// $ npm install ask-sdk-express-adapter
+// $ npm install express
 
-'use strict';
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
 
-const Alexa = require('alexa-sdk');
+const express = require('express');
+const { ExpressAdapter } = require('ask-sdk-express-adapter');
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+const app = express();
 
-const languageStrings = {
-    'en': {
-        translation: {
-            SKILL_NAME: 'Fish Tank',
-            GET_FED_TODAY_MESSAGE: 'Yes, you fed your fish today.',
-            GET_FED_AT_DATE_MESSAGE: 'You last fed your fish 2 days ago.'
-        },
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
-    'de': {
-        translation: {
-            SKILL_NAME: 'Fish Tank auf Deutsch',
-            GET_FED_TODAY_MESSAGE: 'Ja, du hast deine Fische heute gefüttert.',
-            GET_FED_AT_DATE_MESSAGE: 'Du hast deine Fische letztmals vor 2 Tagen gefüttert.'
-        },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
 };
 
-const handlers = {
-    'GetLastFeeding': function () {
-        console.log('GetLastFeeding');
-        this.emit(':tell', this.t('GET_FED_TODAY_MESSAGE'));
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
     },
-    'GetFedToday': function () {
-        console.log('GetFedToday');
-        this.emit(':tell', this.t('GET_FED_TODAY_MESSAGE'));
-    },
-    'GetFedAtDate': function (date) {
-        console.log('GetFedAtDate');
-        this.emit(':tell', this.t('GET_FED_AT_DATE_MESSAGE'));
-    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
 };
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
 };
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+//exports.handler = Alexa.SkillBuilders.custom()
+//    ...
+//    .lambda();
+
+const skillBuilder = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom int$
+    )
+    .addErrorHandlers(
+        ErrorHandler,
+    );
+
+const skill = skillBuilder.create();
+const adapter = new ExpressAdapter(skill, true, true);
+
+app.post('/', adapter.getRequestHandlers());
+app.listen(8080);
 
