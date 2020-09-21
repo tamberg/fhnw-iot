8be6ac4e24fa3283aef88ee7d9cb4589f2aaba08
tamberg <tamberg@yaler.net>
2020-09-21 17:03:18 +0200

    Added example code.

---
+++ 02/Arduino/nRF52840_LightSwitch/nRF52840_LightSwitch.ino
@@ -0,0 +1,21 @@
+int buttonPin = 9; // Grove D4
+int ledPin = 5; // Grove D2
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int state = digitalRead(buttonPin);
+  Serial.println(state);
+//  if (state == HIGH) {
+//    digitalWrite(ledPin, HIGH); // on
+//  } else {
+//      digitalWrite(ledPin, LOW); // off
+//  }
+  digitalWrite(ledPin, state);
+  delay(1);
+}
---
+++ 02/Arduino/nRF52840_LightSwitchStateMachine/nRF52840_LightSwitchStateMachine.ino
@@ -0,0 +1,26 @@
+int buttonPin = 9; // Grove D4
+int ledPin = 5; // Grove D2
+
+int s = 0; // state
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int b = digitalRead(buttonPin);
+  Serial.println(b);
+  if (s == 0 && b == HIGH) { // s is state
+    s = 1; digitalWrite(ledPin, HIGH); // on
+  } else if (s == 1 && b == LOW) {
+    s = 2;
+  } else if (s == 2 && b == HIGH) {
+    s = 3; digitalWrite(ledPin, LOW); // off
+  } else if (s == 3 && b == LOW) {
+    s = 0;
+  }
+  delay(1);
+}
