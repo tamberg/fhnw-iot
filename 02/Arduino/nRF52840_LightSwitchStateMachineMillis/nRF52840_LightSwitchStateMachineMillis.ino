bd4adf30c9e481f802b3770910107dc5726fb2af
tamberg <tamberg@yaler.net>
2020-09-26 12:23:04 +0200

    Added Arduino example.

---
+++ 02/Arduino/nRF52840_LightSwitchStateMachineMillis/nRF52840_LightSwitchStateMachineMillis.ino
@@ -0,0 +1,34 @@
+int buttonPin = 9; // Grove D4
+int ledPin = 5; // Grove D2
+
+long t0;
+int s = 0; // state
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int b = digitalRead(buttonPin);
+  Serial.println(b);
+  if (s == 0 && b == HIGH) {
+    s = 1;
+    digitalWrite(ledPin, HIGH); // on
+  } else if (s == 1 && b == LOW) {
+    s = 2;
+  } else if (s == 2 && b == HIGH) {
+    s = 3;
+    t0 = millis();
+  } else if (s == 3 && b == HIGH && (millis() - t0) >= 1000) {
+    s = 4;
+    digitalWrite(ledPin, LOW); // off
+  } else if (s == 3 && b == LOW) {
+    s = 2;
+  } else if (s == 4 && b == LOW) {
+    s = 0;
+  }
+  delay(1);
+}
