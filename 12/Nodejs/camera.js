c6d3da2f3e9ec714d5961bfe8c543230e3c6f482
tamberg <tamberg@yaler.net>
2020-12-06 15:43:18 +0000

    Cleanup.

--- 12/Nodejs/camera.js
+++ 12/Nodejs/camera.js
@@ -1,17 +1,11 @@
+// $ sudo raspi-config > Interface options > Camera > Enable
+// $ npm install raspicam
+
 const RaspiCam = require("raspicam");
 
 const cam = new RaspiCam({
     mode: "photo",
-    output: "./photo_%d"
+    output: "./photo.jpg"
 });
-
-cam.on("start", function(){
-  console.log("start");
-  //cam.stop();
-});
-
-cam.on("read", (err, timestamp, filename) => { console.log("read"); });
-cam.on("stop", () => { console.log("stop"); });
-cam.on("exit", () => { console.log("exit"); });
 
 cam.start();
