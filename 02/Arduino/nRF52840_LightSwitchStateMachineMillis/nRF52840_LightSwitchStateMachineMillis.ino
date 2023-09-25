f23af7a6c666724d3091b09046d06c757a6425fd
tamberg <tamberg@yaler.net>
2023-09-25 13:33:41 +0200

    Update.

--- 02/Arduino/nRF52840_LightSwitchStateMachineMillis/nRF52840_LightSwitchStateMachineMillis.ino
+++ 02/Arduino/nRF52840_LightSwitchStateMachineMillis/nRF52840_LightSwitchStateMachineMillis.ino
@@ -6,6 +6,10 @@ int ledPin = 5; // Grove D2
 int s = 0; // state
 long t0;
 
+int pressed(int val) {
+  return val == HIGH;
+}
+
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
@@ -16,20 +20,20 @@ void setup() {
 void loop() {
   int b = digitalRead(buttonPin);
   Serial.println(b);
-  if (s == 0 && b == HIGH) {
-    s = 1;
+  if (s == 0 && pressed(b)) {
     digitalWrite(ledPin, HIGH); // on
-  } else if (s == 1 && b == LOW) {
+    s = 1;
+  } else if (s == 1 && !pressed(b)) {
     s = 2;
-  } else if (s == 2 && b == HIGH) {
-    s = 3;
+  } else if (s == 2 && pressed(b)) {
     t0 = millis();
-  } else if (s == 3 && b == HIGH && (millis() - t0) >= 1000) {
-    s = 4;
+    s = 3;
+  } else if (s == 3 && pressed(b) && (millis() - t0) >= 1000) {
     digitalWrite(ledPin, LOW); // off
-  } else if (s == 3 && b == LOW) {
+    s = 4;
+  } else if (s == 3 && !pressed(b)) {
     s = 2;
-  } else if (s == 4 && b == LOW) {
+  } else if (s == 4 && !pressed(b)) {
     s = 0;
   }
   delay(1);
