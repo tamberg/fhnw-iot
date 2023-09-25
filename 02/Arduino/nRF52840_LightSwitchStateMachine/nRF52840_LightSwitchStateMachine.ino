1a265b8fe47c7a20dcff00c235779a53a6d97b3a
tamberg <tamberg@yaler.net>
2023-09-25 13:00:16 +0200

    Update.

--- 02/Arduino/nRF52840_LightSwitchStateMachine/nRF52840_LightSwitchStateMachine.ino
+++ 02/Arduino/nRF52840_LightSwitchStateMachine/nRF52840_LightSwitchStateMachine.ino
@@ -12,18 +12,22 @@ void setup() {
   Serial.begin(115200);
 }
 
+int pressed(int val) {
+  return val == HIGH;
+}
+
 void loop() {
   int b = digitalRead(buttonPin);
   Serial.println(b);
-  if (s == 0 && b == HIGH) {
-    s = 1;
+  if (s == 0 && pressed(b)) {
     digitalWrite(ledPin, HIGH); // on
-  } else if (s == 1 && b == LOW) {
+    s = 1;
+  } else if (s == 1 && !pressed(b)) {
     s = 2;
-  } else if (s == 2 && b == HIGH) {
-    s = 3;
+  } else if (s == 2 && pressed(b)) {
     digitalWrite(ledPin, LOW); // off
-  } else if (s == 3 && b == LOW) {
+    s = 3;
+  } else if (s == 3 && !pressed(b)) {
     s = 0;
   }
   delay(1);
